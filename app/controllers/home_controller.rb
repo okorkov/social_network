33256377bce8fb0b6318fac897158833171cae7e
okorkov <alex.okorkov@gmail.com>
2020-12-22 22:03:17 -0800

    added posts box in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     if self.logged_in?
       @user = self.current_user
       @location = CurrentLocation.location_info
+      @posts = Post.where(user_id: session[:user_id]).reverse
       erb :"/home/home", :layout => :"/layout/layout"
     else
       redirect "/error/you're currently not logged in"
