e3d98b50a8f662e70ca16b620ee2bc748078129d
okorkov <alex.okorkov@gmail.com>
2020-12-22 16:22:58 -0800

    log in and sign up controllers, redirect to error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,11 +19,33 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    puts params
-    "redirect"
+    if User.find_by(username: params[:username]) != nil
+      redirect '/error/this user name already exists'
+    end
+    if User.find_by(email: params[:email]) != nil
+      redirect '/error/this email already been used'
+    end
+      @user = User.new(params)
+      if @user.save
+        redirect "/home"
+      else
+        redirect "/error/we weren't able to register you"
+      end
   end
 
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+  
+  get '/error/:err' do
+    @message = params[:err]
+    erb :login_signup_error
+  end
 
 end
 
 
+# "username"=>"okorkov", "email"=>"ceo@socialnetwork.com", "password"=>"1827qaws", 
+# "first_name"=>"alex", "last_name"=>"Okarkau", "birthday"=>"1992-04-27"}
+#<Rack::Session::Abstract::PersistedSecure::SecureSessionHash:0x00007fb3012a1ff0>
