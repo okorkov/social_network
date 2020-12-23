a48275f73cac3c2cffe2a20c0a63695ce6bcf13e
okorkov <alex.okorkov@gmail.com>
2020-12-22 23:28:28 -0800

    added profile controller and edit profile erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,21 @@ class HomeController < ApplicationController
       @user = self.current_user
       @location = CurrentLocation.location_info
       @posts = Post.where(user_id: session[:user_id]).reverse
-      erb :"/home/home", :layout => :"/layout/layout"
+      erb :"/pages/home", :layout => :"/layout/layout"
     else
       redirect "/error/you're currently not logged in"
+    end
+  end
+
+  get '/profile/:id' do
+    id = params[:id]
+    if User.find_by(id: id)
+      @user = User.find(id)
+      @location = CurrentLocation.location_info
+      @posts = Post.where(user_id: id).reverse
+      erb :"/pages/home", :layout => :"/layout/layout"
+    else
+      redirect '/error/there is no user with this id'
     end
   end
 end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,3 +1,16 @@
 class ProfileController < ApplicationController
+
+  get '/profile' do
+    @user = self.current_user
+    erb :"/pages/profile", :layout => :"/layout/layout"
+  end
+
+  post '/profile/:id' do
+    @user = self.current_user
+    # @user.title = params[:title]
+    # @user.content = params[:content]
+    # @user.save
+    redirect "/home/"
+  end
   
 end
