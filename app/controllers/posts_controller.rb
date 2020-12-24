d4cd66e9eab4afbdd7f2596fbc42b11ec674c172
okorkov <alex.okorkov@gmail.com>
2020-12-23 22:41:09 -0800

    updated posts appearance in home erb(limit added)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   
   get '/posts' do
-    
+    @user = self.current_user
+
+    erb :"/pages/posts", :layout => :"/layout/layout"
+  end
+
+  post '/posts' do
+    Post.create(params)
+    redirect '/home'
   end
 
 end
